89f5f187e4d4d3855b1c1a2cb0ff66e73ca57d13
Leonardo Celente <leocelente@gmail.com>
2020-09-16 01:05:55 -0300

    Start of command interface
    Sadly I'm not using compiled time regex, but thats ok
    
    
    Former-commit-id: 3c55e6abc9ce1710e48aa776c6a4601f1ad5500c

--- collector/src/collector.cpp
+++ collector/src/collector.cpp
@@ -1,11 +1,51 @@
 #include "../include/collector.h"
 #include <fstream>
 #include <iostream>
+#include <regex>
 
 int main(int argc, const char **argv) {
   std::cout << "Loading NER Model..." << std::fflush;
   auto extractor = nlp::EntityExtractor("./models/ner_model.dat");
   std::cout << "OK" << std::endl;
+
+  std::string cmd;
+  std::string arg;
+  const std::regex is_exit("^(exit|EXIT)$");
+  const std::regex is_add("^(add|ADD)");
+  const std::regex is_url("([[:graph:]]+)");
+  const std::regex is_find("^(search|SEARCH)");
+  const std::regex is_term("([[:alnum:]]+)");
+  std::smatch cmd_match;
+  std::smatch arg_match;
+
+  do {
+    std::cout << ">";
+    std::cin >> cmd;
+    std::cin >> arg;
+    if (std::regex_match(cmd, cmd_match, is_add) &&
+        std::regex_match(arg, arg_match, is_url)) {
+
+      std::ssub_match action_match = cmd_match[0];
+      std::string action = action_match.str();
+      std::cout << "  [adding]: '" << action << "'";
+      std::ssub_match url_match = arg_match[0];
+      std::string url = url_match.str();
+      std::cout << "  url: " << url << '\n';
+
+    } else if (std::regex_match(cmd, cmd_match, is_find) &&
+               std::regex_match(arg, arg_match, is_term)) {
+
+      std::ssub_match action_match = cmd_match[0];
+      std::string action = action_match.str();
+      std::cout << "  [searching]: '" << action << "'";
+      std::ssub_match url_match = arg_match[0];
+      std::string url = url_match.str();
+      std::cout << "  term: " << url << '\n';
+
+    } else {
+      std::cout << "[*] Not a valid command!\n";
+    }
+  } while (!std::regex_match(cmd, cmd_match, is_exit));
 
   for (size_t i = 0; i < 2; i++) {
     std::string filename{"./sample_txt/IIC.txt"};
